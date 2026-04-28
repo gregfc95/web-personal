793c87421a9b84537a5a30e4fdf07fd2f182d4a9
gregfc95 <josegfc95@gmail.com>
2026-04-27 21:26:15 -0300

    chore initial files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gregfc95.github.io/',
   prefetch: true,
   integrations: [tailwind()],
 });
